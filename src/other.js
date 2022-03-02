16b19a0b2087f78dfbe9ee2fdf2315de066e889c
carolainseija <carolainsilva1@gmail.com>
2022-03-02 10:57:03 -0300

    cooomit initial2

--- src/other.js
+++ src/other.js
@@ -1,8 +1,8 @@
 import React from "react";
 import db from "./utils/firebase";
-import { doc, getDoc, getDocs } from "firebase/firestore";
+import { doc, getDoc, getDocs, collection } from "firebase/firestore";
 
-const getData = async () => {
+const getoNEData = async () => {
     console.log("funcion")
     const docRef = doc(db, "enero", "K1Z8z344zUZ3045r12M5");
     const docSnap = await getDoc(docRef);
@@ -17,7 +17,21 @@ console.log(docSnap)
     console.log(true);
 };
 
+//tyodo los datos
+const getData = async () => {
+    console.log("funcion")
+    const querySnapshot = await getDocs(collection(db, "enero"));
+    querySnapshot.forEach((doc) => {
+        console.log(doc.id, " => ", doc.data());
+    });
+    //PPONERLOS EN UN ARRRAY
+    /**
+     const array = [];
+     array.push((doc.id, " => ", doc.data())
+     */
+};
 
+//solo uN DATO
 const Other = () => {
     return (
         <>
